3b898c509ff18be94a2a9885bc7af8825b4b5bd9
Dhaiwat <dhaiwatpandya@gmail.com>
2023-12-10 01:26:42 +0530

    Update decode.tsx

--- pages/decode.tsx
+++ pages/decode.tsx
@@ -8,12 +8,12 @@ const Index = () => {
 	const [qrResult, setQrResult] = useState<string>()
 	const [decompressed, setDecompressed] = useState<string>()
 
-	// useEffect(() => {
-	// 	if (qrResult) {
-	// 		const decompressed = JSON.parse(qrResult)
-	// 		setDecompressed(JSON.stringify(decompressed, null, 2))
-	// 	}
-	// }, [qrResult])
+	useEffect(() => {
+		if (qrResult) {
+			const decompressed = JSON.parse(qrResult)
+			setDecompressed(JSON.stringify(decompressed, null, 2))
+		}
+	}, [qrResult])
 
 	return (
 		<Page>
