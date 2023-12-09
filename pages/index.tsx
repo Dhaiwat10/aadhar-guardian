e2935f7c78fa47f0f718cb711c0849c83f698d8f
Dhaiwat <dhaiwatpandya@gmail.com>
2023-12-09 23:39:59 +0530

    add animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
 import { Button } from '@/components/button'
 import Page from '@/components/page'
 import { useRouter } from 'next/router'
+import { animate } from 'framer-motion'
 
 const Index = () => {
-	const router = useRouter()
-
 	return (
 		<Page>
-			<img src='/images/shapes.svg' alt='Shapes' className='absolute -left-40' />
-			<p className='text-5xl font-medium text-black mt-auto'>
+			<img
+				src='/images/shapes.svg'
+				alt='Shapes'
+				className='absolute scale-125 my-auto mx-auto -left-40 self-center'
+				id='shapes'
+			/>
+			<p className='text-4xl font-medium text-black mt-auto'>
 				Secure Your Aadhaar
 			</p>
-			<Button onClick={() => router.push('/upload-aadhar')} className='mt-12'>
+			<Button
+				onClick={() => {
+					const img = document.getElementById('shapes') as HTMLImageElement
+					console.log({
+						img,
+					})
+					animate(img, { scale: 2, rotate: 45, left: '0px' }, { duration: 0.5 })
+				}}
+				className='mt-12'
+			>
 				Let&apos;s get started
 			</Button>
 		</Page>
