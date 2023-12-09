6f14f067c45a50a9eacc61f8fce57665d15178d1
Dhaiwat <dhaiwatpandya@gmail.com>
2023-12-09 23:16:27 +0530

    add img

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
 import { Button } from '@/components/button'
 import Page from '@/components/page'
-import Section from '@/components/section'
-import { LogInWithAnonAadhaar, useAnonAadhaar } from 'anon-aadhaar-react'
 import { useRouter } from 'next/router'
-import { useAccount } from 'wagmi'
 
 const Index = () => {
 	const router = useRouter()
 
 	return (
 		<Page>
+			<img src='/images/shapes.svg' alt='Shapes' className='absolute -left-40' />
 			<p className='text-5xl font-medium text-black mt-auto'>
 				Secure Your Aadhaar
 			</p>
-			<Button onClick={() => router.push('/upload-aadhar')} className='mt-12'>Let&apos;s get started</Button>
+			<Button onClick={() => router.push('/upload-aadhar')} className='mt-12'>
+				Let&apos;s get started
+			</Button>
 		</Page>
 	)
 }
