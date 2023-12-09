86ccfed7c1c6a1003fdbca5214e5434ab4206475
Dhaiwat <dhaiwatpandya@gmail.com>
2023-12-09 11:55:59 +0530

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import '@/styles/globals.css'
+import { AnonAadhaarProvider } from 'anon-aadhaar-react'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<ThemeProvider
-			attribute='class'
-			defaultTheme='system'
-			disableTransitionOnChange
-		>
-			<Component {...pageProps} />
-		</ThemeProvider>
+		<AnonAadhaarProvider _appId={process.env.NEXT_PUBLIC_APP_ID as string}>
+			<ThemeProvider
+				attribute='class'
+				defaultTheme='system'
+				disableTransitionOnChange
+			>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</AnonAadhaarProvider>
 	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,29 @@
 import Page from '@/components/page'
 import Section from '@/components/section'
+import { LogInWithAnonAadhaar, useAnonAadhaar } from 'anon-aadhaar-react'
+import QRCode from 'react-qr-code'
 
-const Index = () => (
-	<Page>
-		<Section>
-			<h2 className='text-xl font-semibold text-zinc-800 dark:text-zinc-200'>
-				We grow a lot of rice.
-			</h2>
+const Index = () => {
+	const [aadharState] = useAnonAadhaar()
 
-			<div className='mt-2'>
-				<p className='text-zinc-600 dark:text-zinc-400'>
-					You love rice, and so does the rest of the world. In the crop year
-					2008/2009, the milled rice production volume amounted to over{' '}
-					<span className='font-medium text-zinc-900 dark:text-zinc-50'>
-						448 million tons
-					</span>{' '}
-					worldwide.
-				</p>
+	console.log({
+		aadharState,
+	})
 
-				<br />
+	return (
+		<Page>
+			<Section>
+				<LogInWithAnonAadhaar />
 
-				<p className='text-sm text-zinc-600 dark:text-zinc-400'>
-					<a
-						href='https://github.com/mvllow/next-pwa-template'
-						className='underline'
-					>
-						Source
-					</a>
-				</p>
-			</div>
-		</Section>
-	</Page>
-)
+				{aadharState.status === 'logged-in' && (
+					<div>
+						<pre>{JSON.stringify(aadharState.pcd, null, 2)}</pre>
+						<QRCode value={JSON.stringify(aadharState.pcd)} />
+					</div>
+				)}
+			</Section>
+		</Page>
+	)
+}
 
 export default Index
