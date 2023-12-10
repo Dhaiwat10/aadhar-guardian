cefa9c6431d7c1949c06725d0bff907979fb9db6
Dhaiwat <dhaiwatpandya@gmail.com>
2023-12-10 08:39:36 +0530

    final

--- pages/decode.tsx
+++ pages/decode.tsx
@@ -47,8 +47,41 @@ const Index = () => {
 		<Page>
 			{pageState === PageState.scanning && (
 				<>
-					<h1>Decode</h1>
-					<span>isMobile: {isMobile}</span>
+					<h1 className='text-3xl font-medium'>Merchant view</h1>
+					<ol className='text-gray-600 text-sm'>
+						<li>1. Select the Aadhar fields you want to verify.</li>
+						<li>
+							2. Ask the citizen to generate a proof and a QR code from the
+							Aadhaar Shield app.
+						</li>
+						<li>3. Scan the QR code.</li>
+						<li>
+							4. The app will tell you if the presented proof is valid or not.
+						</li>
+					</ol>
+
+					<hr className='my-4' />
+
+					<label className='text-black rounded-lg font-medium'>
+						Fields you want to verify:
+					</label>
+					<div className='flex flex-row'>
+						<label>
+							<input type='checkbox' defaultChecked disabled />
+							<span className='ml-2'>Age</span>
+						</label>
+						<label className='ml-4'>
+							<input type='checkbox' disabled />
+							<span className='ml-2 opacity-20'>Name</span>
+						</label>
+						<label className='ml-4'>
+							<input type='checkbox' disabled />
+							<span className='ml-2 opacity-20'>Location</span>
+						</label>
+					</div>
+
+					<h3 className='text-black font-medium mt-4'>Scan the QR:</h3>
+
 					<QrReader
 						constraints={{
 							facingMode: isMobile ? 'environment' : 'user',
@@ -65,14 +98,19 @@ const Index = () => {
 
 			{pageState === PageState.verifyingProof && (
 				<div className='flex flex-col'>
-					<h1>Verifying Proof</h1>
+					<h1>Verifying Proof...</h1>
 				</div>
 			)}
 
 			{pageState === PageState.proofVerified && (
 				<div className='flex flex-col items-center'>
-					<h1 className='text-4xl font-medium'>Proof Verified</h1>
-					<p>{proofVerifiedRes ? 'true' : 'false'}</p>
+					<h1 className='text-4xl font-medium'>
+						{proofVerifiedRes ? 'Proof verified!' : 'Proof invalid!'}
+					</h1>
+
+					<button className='mt-4' onClick={() => window.location.reload()}>
+						Scan another QR
+					</button>
 				</div>
 			)}
 		</Page>
